e2b893eb0ffb7228d3a1769f78d6dc0fdb9448a9
Akil kumar <akilkumar96@gmail.com>
2018-11-26 21:56:25 +0530

    Project setup

--- app/src/main/java/com/example/back4app/userregistrationexample/UserList.java
+++ app/src/main/java/com/example/back4app/userregistrationexample/UserList.java
@@ -1,127 +1,27 @@
 package com.example.back4app.userregistrationexample;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.MediaStore;
-import android.support.annotation.MainThread;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserList extends AppCompatActivity {
-    public void getPhoto() {
-
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, 1);
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 1) {
-
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                getPhoto();
-
-            }
-
-
-        }
-
-    }
-
-
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-
-            Uri selectedImage = data.getData();
-
-            try {
-
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
-
-                Log.i("Photo", "Received");
-
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-
-                byte[] byteArray = stream.toByteArray();
-
-                ParseFile file = new ParseFile("image.png", byteArray);
-
-                ParseObject object = new ParseObject("Image");
-
-                object.put("image", file);
-
-                object.put("username", ParseUser.getCurrentUser().getUsername());
-
-                object.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-
-                        if (e == null) {
-
-                            Toast.makeText(UserList.this, "Image Shared!", Toast.LENGTH_SHORT).show();
-
-                        } else {
-
-                            Toast.makeText(UserList.this, "Image could not be shared - please try again later.", Toast.LENGTH_SHORT).show();
-
-                        }
-
-                    }
-                });
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
-
         setTitle("User Feed");
 
         final ArrayList<String> usernames = new ArrayList<String>();
@@ -173,21 +73,5 @@ public class UserList extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
 }
